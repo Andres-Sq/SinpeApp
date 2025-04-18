5ec60cf2287dc3703cc03ef0e1e6ed1fab41f078
Andres-Sq <asu0090@gmail.com>
2025-04-18 14:00:19 -0600

    clean code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -32,16 +32,14 @@ function sendSMS() {
       .then(response => response.json())
       .then(data => {
         console.log("Server response:", data);
-        // Message to show after successful SMS generation
         const alertMsg = isEnglish ? "Message generated successfully." : "Mensaje generado con éxito.";
         alert(alertMsg);
-        window.location.href = ".../../checkpages/success.html"; // Redirect to success page
+        window.location.href = ".../../checkpages/success.html";
       })
       .catch(error => {
         console.error("Error:", error);
-        //Message to show if there was an error generating the SMS
         const alertMsg = isEnglish ? "There was an error sending the message." : "Se ha producido un error al enviar el mensaje.";
         alert(alertMsg);
-        window.location.href = ".../../checkpages/error.html"; // Redirect to error page
+        window.location.href = ".../../checkpages/error.html";
       });
   }
