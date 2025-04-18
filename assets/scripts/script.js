940249abf603ad890430d2e049e1964ae5447781
Andres-Sq <asu0090@gmail.com>
2025-04-18 15:15:39 -0600

    before drop api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,47 @@
-const numeroInput=document.getElementById("phone_number");
-numeroInput.addEventListener("input",(function(e){let valor=e.target.value.replace(/\D/g,"");
-if(valor.length>4){valor=valor.slice(0,4)+"-"+valor.slice(4,8)}e.target.value=valor}));
+const numeroInput = document.getElementById('phone_number');
 
-function sendSMS() {
-    const isEnglish = window.location.pathname.includes("main_en");
-    const amount = document.getElementById("amount").value;
-    const phone_number = document.getElementById("phone_number").value.replace(/-/g, "");
-    const details = document.getElementById("details").value;
-    const bank = document.getElementById("bank").value;
-  
-    if (!bank || !phone_number || !amount || !details) {
-        const alertMsg = isEnglish ? "Please complete all fields." : "Por favor completa todos los campos.";
-        alert(alertMsg);
-        return;
-    }
-  
-    const datos = {
-        bank,
-        phone_number,
-        amount,
-        details
-    };
-  
-    fetch("sinpe-api-cwgbcxdbekeae2cv.canadacentral-01.azurewebsites.net", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer andres`
-      },
-      body: JSON.stringify(datos)
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Server response:", data);
-        const alertMsg = isEnglish ? "Message generated successfully." : "Mensaje generado con éxito.";
-        alert(alertMsg);
-        window.location.href = ".../../checkpages/success.html";
-      })
-      .catch(error => {
-        console.error("Error:", error);
-        const alertMsg = isEnglish ? "There was an error sending the message." : "Se ha producido un error al enviar el mensaje.";
-        alert(alertMsg);
-        window.location.href = ".../../checkpages/error.html";
-      });
+numeroInput.addEventListener('input', function (e) {
+  let valor = e.target.value.replace(/\D/g, '');
+
+  if (valor.length > 4) {
+    valor = valor.slice(0, 4) + '-' + valor.slice(4, 8);
   }
+
+    e.target.value = valor;
+  });
+
+function createSMS() {
+  const isEnglish = window.location.pathname.includes("main_en");
+
+  const bank = document.getElementById("bank").value;
+  const phone_number = document.getElementById("phone_number").value.replace(/-/g, '');
+  const amount = document.getElementById("amount").value;
+  const details = document.getElementById("details").value;    
+
+  if (!bank || !phone_number || !amount || !details) {
+     alertMsg = isEnglish
+    ? "Please complete all fields."
+    : "Por favor completa todos los campos.";
+   alert(alertMsg);
+  return;
+  }
+
+    const mensaje = `Pase ${amount} ${phone_number} ${details}`;
+    const link = `sms:${bank}?&body=${encodeURIComponent(mensaje)}`;
+  
+    window.location.href = link;
+  }
+ 
+document.addEventListener("DOMContentLoaded", () => {
+document.getElementById("year").textContent = new Date().getFullYear()});
+
+//Disable
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12") e.preventDefault();
+  if (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "J")) e.preventDefault();
+  if (e.ctrlKey && e.key === "u") e.preventDefault();
+  if (e.ctrlKey && e.key === "s") e.preventDefault();
+});
