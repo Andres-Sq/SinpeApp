e53f51e25389f21bf2805d745f9dac496c2704a2
Andres-Sq <asu0090@gmail.com>
2025-05-04 15:59:54 -0600

    mobile fix v13

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,11 @@
 export const onRequest = async (context) => {
   const url = new URL(context.request.url);
 
+  // Permitir acceso a /checkpages/success.html sin necesidad del parámetro "client"
+  if (url.pathname === "/checkpages/success.html") {
+    return context.next();
+  }
+
   // Permitir acceso a archivos estáticos
   if (
     url.pathname.startsWith('/assets/images') ||
@@ -9,7 +14,7 @@ export const onRequest = async (context) => {
     return context.next();
   }
 
-  // Verificar parámetro 'client' en otras rutas
+  // Verificar parámetro "client" en otras rutas
   if (!url.searchParams.has("client")) {
     return new Response("Acceso no autorizado", { status: 403 });
   }
