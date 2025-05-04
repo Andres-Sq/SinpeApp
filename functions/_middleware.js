cdc5df91987296585969cac67b210b5ccd7e01ff
Andres-Sq <asu0090@gmail.com>
2025-05-04 12:18:26 -0600

    Fix load page bug

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,13 +1,20 @@
 export const onRequest = async ({ request, next }) => {
-    const url = new URL(request.url);
-  
-    // Permitir acceso solo si incluye ?client= en la URL
-    if (!url.searchParams.has("client")) {
-      return new Response("Acceso no autorizado", {
-        status: 403,
-        headers: { "Content-Type": "text/plain" }
-      });
-    }
-  
-    return next(); // continuar con la carga del sitio
-  };  
+  const url = new URL(request.url);
+
+  // Permitir recursos estáticos sin client=
+  const isStatic = url.pathname.startsWith("/assets/") || url.pathname.endsWith("/favicon.ico");
+
+  if (isStatic) {
+    return next(); // permitir acceso sin client
+  }
+
+  // Bloquear el resto si no tiene ?client
+  if (!url.searchParams.has("client")) {
+    return new Response("Acceso no autorizado", {
+      status: 403,
+      headers: { "Content-Type": "text/plain" }
+    });
+  }
+
+  return next(); // continuar con la carga del sitio
+};
